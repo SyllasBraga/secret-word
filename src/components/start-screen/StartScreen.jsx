d3f7c811ab1fdfb7df386b8dfa54af250eab03c1
Syllas <syllasbraga2@gmail.com>
2025-01-01 15:55:24 -0300

    feat: buttons actions

--- src/components/start-screen/StartScreen.jsx
+++ src/components/start-screen/StartScreen.jsx
@@ -1,11 +1,11 @@
 import "./StartScreen.css"
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
     return (
         <div className="start">
             <h1>Secret Word</h1>
-            <p>CLick here to start to the game</p>
-            <button>Start the game</button>
+            <p>Click the button bellow to start to the game</p>
+            <button onClick={startGame}>Start the game</button>
         </div>
     )
 }
