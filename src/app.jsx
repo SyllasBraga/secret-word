c8186b72f8ae367da4b3817c8488e07c044ce505
Syllas <syllasbraga2@gmail.com>
2025-01-01 15:34:26 -0300

    feat: StartScreen component

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,23 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import './app.css';
+import StartScreen from './components/start-screen/StartScreen';
+import { useCallback, useEffect, useState } from 'preact/hooks';
+import { wordsList } from './data/words'
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+]
 
 export function App() {
-  const [count, setCount] = useState(0)
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <div className="App">
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <StartScreen />}
+      {gameStage === 'end' && <StartScreen />}
+    </div>
   )
 }
---
+++ src/components/start-screen/StartScreen.jsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+    return (
+        <div className="start">
+            <h1>Secret Word</h1>
+            <p>CLick here to start to the game</p>
+            <button>Start the game</button>
+        </div>
+    )
+}
+
+export default StartScreen
