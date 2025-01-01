75645511b3784a3ac65b11c576cc0ece5d1426ce
Syllas <syllasbraga2@gmail.com>
2025-01-01 14:49:57 -0300

    feat: Words list

---
+++ src/data/words.jsx
@@ -0,0 +1,13 @@
+export const wordsList = {
+    car: ["Engine", "Door", "Hood", "Tire", "Antenna", "Windshield", "Headlights", "Steering", "Seat", "Exhaust"],
+    fruit: ["Banana", "Apple", "Pear", "Papaya", "Orange", "Grapes", "Pineapple", "Watermelon", "Peach", "Strawberry"],
+    body: ["Arm", "Leg", "Brain", "Neck", "Eyes", "Hands", "Feet", "Heart", "Lungs", "Stomach"],
+    computer: ["Mouse", "Keyboard", "Monitor", "Case", "CPU", "RAM", "Hard Drive", "Graphics Card", "Screen", "Speakers"],
+    programming: ["Language", "Framework", "JavaScript", "React", "Python", "CSS", "HTML", "Node.js", "API", "Git"],
+    food: ["Rice", "Beans", "Meat", "Milk", "Egg", "Chicken", "Bread", "Cheese", "Fruit", "Vegetables"],
+    animals: ["Lion", "Elephant", "Tiger", "Bear", "Zebra", "Giraffe", "Monkey", "Kangaroo", "Panda", "Cheetah"],
+    colors: ["Red", "Blue", "Green", "Yellow", "Black", "White", "Purple", "Pink", "Brown", "Orange"],
+    countries: ["Brazil", "USA", "Canada", "Germany", "Australia", "France", "Japan", "China", "India", "Italy"],
+    sports: ["Soccer", "Basketball", "Tennis", "Baseball", "Volleyball", "Swimming", "Cycling", "Rugby", "Golf", "Boxing"],
+    weather: ["Sunny", "Rainy", "Cloudy", "Windy", "Snowy", "Foggy", "Stormy", "Hot", "Cold", "Humid"],
+};
